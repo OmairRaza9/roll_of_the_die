adfe5dee8a9a850f111e5605156f617f4effe00d
Omair <omairraza9@gmail.com>
2017-11-05 20:17:38 -0500

    permutations done- i looked at the answers

---
+++ permutations.rb
@@ -0,0 +1,11 @@
+dice_1 = Random.rand(6) + 1
+dice_2 = Random.rand(6) + 1
+
+#I have to roll every possible pair
+#They then need to be sorted
+
+(1..6).each do |die1|
+  (1..6).each do |die2|
+  puts "#{die1}, #{die2}"
+  end
+end
